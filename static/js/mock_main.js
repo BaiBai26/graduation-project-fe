107e9c62eb62fae2a45156710f490b3f2fddbdce
jackey.dou <jackey.dou@bytedance.com>
2019-03-17 00:16:35 +0800

    feat: 主页面更新

---
+++ static/js/mock_main.js
@@ -0,0 +1,8 @@
+export const SHAXIANG_NUMBER = 4
+export const PHY_MACHINE_NUMBER = 8
+export const VIRTUAL_MACHINE_NUMBER = 8
+export const SYS_HEALTH = 87
+export const RIZHI_PER_SECOND = 20
+export const SECU_EVENT_NUMBER = 69
+export const GAOJING_EVENT_NUMBER = 44
+export const UNDONE_EVENT_NUMBER = 3
